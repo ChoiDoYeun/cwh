3fe6d39742c959808cf7a428223a32092de606d7
ChoiDoYeun <fff0803@naver.com>
2024-01-22 20:59:34 +0900

    입차대기장소로 이동 기능 추가

--- controls/run.py
+++ controls/run.py
@@ -2,11 +2,13 @@
 import threading
 from qr_processor import process_qr_code
 from motor.move_position import move_position
+import time
 
 def run(warehouse):
     print("Run 시작")
     
-    move_position(0,0,0) # 초기상태로 이동
+    move_position(225,350,50) # 입차대기장소로 이동
+    time.sleep(0.5)
     
     # 멀티스레딩 #qr코드 인식 및 저장장소 처리
     qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,))
