50fe7f135fcb29e56f7eb45c456f775bb74e46c6
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-22 19:36:14 +0900

    Update in_out_mode.py

--- motor/in_out_mode.py
+++ motor/in_out_mode.py
@@ -3,18 +3,28 @@ from warehouse_class import Warehouse
 from motor.move_position import move_position
 from motor.move_item import move_item_high,move_item_low
 
-def input_mode(location): 
+def input_mode(x,y,z): 
     warehouse = Warehouse()
-    x,y,z = warehouse.calculate_coordinates(location) # 차량이 저장된 위치의 x,y,z값 계산
     move_item_low() # 입차대기장소로 와있는 상태에서 차량을 함으로 이동 # move_item_low 와 move_item_high의 위치는 바뀔수있음
+    time.sleep(1)
+    print("입차중 : 함에 이동완료")
     move_position(x,y,z) # 입차장소로 이동
+    time.sleep(1)
+    move_position(225,350,50) # 입차대기장소로 이동
+    time.sleep(1)
     move_item_high()    # 차를 함에서 입차장소로 이동
-    # move_position(입차대기장소의 x,y,z) # 입차대기장소로 이동
+    print("입차 완료")
+    time.sleep(1)
     
-def output_mode(location):
+    
+def output_mode(x,y,z):
     warehouse = Warehouse()
-    x,y,z = warehouse.calculate_coordinates(location)
-    move_item_high() # move_item_low 와 move_item_high의 위치는 바뀔수있음
-    move_position(x,y,z)
+    move_position(x,y,z) # 저장장소로 이동
+    time.sleep(1)
+    move_item_high() # 꺼내기
+    print("출차중 : 함에 이동완료")
+    time.sleep(1)
+    move_position(225,350,50) # 출차장소로 이동
+    time.sleep(1)
     move_item_low()
-    # move_position(출차대기장소의 x,y,z) # 출차대기장소로 이동
+    print("출차중 : 출차완료")
