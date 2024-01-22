fb97486b488357b4f014e349328a65c821272575
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-01-22 19:36:35 +0900

    Update move_item.py

--- motor/move_item.py
+++ motor/move_item.py
@@ -4,13 +4,17 @@ from motor.move_motor import move_motor
 from motor.config import initialize
 
 def move_item_high():
-    A_STEP, A_DIR = initialize()
+    X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP, Z_DIR, A_STEP,A_DIR  = initialize()
     steps = 500 # 예시
     direction = "HIGH" # 예시
+    print("A모터 동작중")
     move_motor(A_STEP, A_DIR, steps, direction)
+    print("A모터 동작완료")
     
 def move_item_low():
-    A_STEP, A_DIR = initialize()
+    X_STEP, X_DIR, Y_STEP, Y_DIR, Z_STEP, Z_DIR, A_STEP,A_DIR  = initialize()
     steps = 500 # 예시
     direction = "LOW" # 예시
+    print("A모터 동작중")
     move_motor(A_STEP, A_DIR, steps, direction)
+    print("A모터 동작완료")
