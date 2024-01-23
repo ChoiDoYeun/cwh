f36f0c2c4c8cb0b40f447cbcac3830d3000e3a67
ChoiDoYeun <fff0803@naver.com>
2024-01-23 14:57:21 +0900

    멀티스레딩 삭제. 멀티스레딩은 toggle_mode로 이동

--- controls/run.py
+++ controls/run.py
@@ -1,16 +1,9 @@
 # run.py
-import threading
-from qr_processor import process_qr_code
+
 from motor.move_position import move_position_back
 import time
 
-def run(warehouse):
+def run():
     print("Run 시작")
-    
     move_position_back(225,350,50) # 입차대기장소로 이동
     time.sleep(0.5)
-    
-    # 멀티스레딩 #qr코드 인식 및 저장장소 처리
-    qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,))
-    qr_thread.daemon = True
-    qr_thread.start()
