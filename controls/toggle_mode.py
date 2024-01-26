f11ed62d879aaac75493a1d783c4422e8bc40b67
ChoiDoYeun <fff0803@naver.com>
2024-01-26 16:04:14 +0900

    input off 정상동작

--- controls/toggle_mode.py
+++ controls/toggle_mode.py
@@ -1,22 +1,28 @@
 # toggle_mode.py
 import threading
-from qr_processor import process_qr_code
+from qr_processor import process_qr_code_input,process_qr_code_output, StopThreadException
 
-global stop_thread
-stop_thread = False
-qr_thread = None  # 스레드를 전역 변수로 선언
+global qr_thread
+qr_thread = None
 
 def input_on(warehouse):
-    global stop_thread, qr_thread
-    stop_thread = False
-    print("Input Mode ON")
-
-    qr_thread = threading.Thread(target=process_qr_code, args=(warehouse,))
+    global qr_thread
+    qr_thread = threading.Thread(target=process_qr_code_input, args=(warehouse,))
     qr_thread.start()
     
 def input_off():
-    global stop_thread, qr_thread
-    stop_thread = True
+    global qr_thread
     if qr_thread:
-        qr_thread.join()  # 스레드가 종료될 때까지 기다립니다
-    print("Input Mode OFF")
+        qr_thread.raise_exception(StopThreadException)  # 예외를 발생시킵니다.
+        qr_thread.join()  # 스레드가 종료될 때까지 기다립니다.
+        
+
+# 스레드 객체에 예외를 발생시키는 메서드 추가
+def raise_exception(self, exc):
+    import ctypes
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(self.ident), ctypes.py_object(exc))
+    if res > 1:
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(self.ident, 0)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+
+threading.Thread.raise_exception = raise_exception
--- qr_processor.py
+++ qr_processor.py
@@ -1,30 +1,58 @@
+# process_qr_code.py
 import time
 from camera import read_qr_code
-from motor.in_out_mode import input_mode
+from motor.in_out_mode import input_mode,output_mode
 
 stop_thread = False
-
+class StopThreadException(Exception):
+    """커스텀 예외 클래스로 스레드 종료를 위해 사용됩니다."""
+    pass
 
 # qr코드 정보처리
-def process_qr_code(warehouse):
-    global stop_thread
-    qr_code_image_path = '세단_QR_Code.PNG'
-    while not stop_thread:
-        #capture_image(qr_code_image_path)
-        qr_data = read_qr_code(qr_code_image_path)
-        if qr_data:
-            cars = qr_data.split('\n')
-            for car in cars:
-                if car:
-                    location = warehouse.store_item(car)
-                    if location:
-                        print(f"{car}가 {location}에 저장되었습니다.")
-                        x, y, z = warehouse.calculate_coordinates(location)
-                        input_mode(x,y,z)
-                    else:
-                        print(f"{car}를 저장할 공간이 없습니다.")
-        else:
-            print("QR 코드를 읽을 수 없습니다.")
+def process_qr_code_input(warehouse):
+    while True:
+        try:
+            #capture_image(qr_code_image_path)
+            qr_code_image_path = '세단_QR_Code.PNG'
+            qr_data = read_qr_code(qr_code_image_path)
+            if qr_data:
+                cars = qr_data.split('\n')
+                for car in cars:
+                    if car:
+                        location = warehouse.store_item(car)
+                        if location:
+                            print(f"{car}가 {location}에 저장되었습니다.")
+                            x, y, z = warehouse.calculate_coordinates(location)
+                            input_mode(x,y,z)
+                        else:
+                            print(f"{car}를 저장할 공간이 없습니다.")
+            else:
+                print("QR 코드를 읽을 수 없습니다.")
+                        # 15초마다 동작
+            time.sleep(5)
+        except StopThreadException:
+            break  # 예외가 발생하면 루프를 종료합니다.
         
-        # 15초마다 동작
-        time.sleep(5)
+def process_qr_code_output(warehouse):
+    while True:
+        try:
+            #capture_image(qr_code_image_path)
+            qr_code_image_path = 'Combined_Cars_QR_Code.PNG'
+            qr_data = read_qr_code(qr_code_image_path)
+            if qr_data:
+                cars = qr_data.split('\n')
+                for car in cars:
+                    if car:
+                        location = warehouse.store_item(car)
+                        if location:
+                            print(f"{car}가 {location}에 저장되어있습니다.")
+                            x, y, z = warehouse.calculate_coordinates(location)
+                            output_mode(x,y,z)
+                        else:
+                            print(f"{car}를 저장할 공간이 없습니다.")
+            else:
+                print("QR 코드를 읽을 수 없습니다.")
+                        # 15초마다 동작
+            time.sleep(5)
+        except StopThreadException:
+            break  # 예외가 발생하면 루프를 종료합니다.
